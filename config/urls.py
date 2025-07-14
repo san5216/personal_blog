f88e1fcdd0ac0bc69868d07fcdf7248604119b17
san5216 <mster.norris@gmail.com>
2025-07-13 21:21:02 -0500

    Include user urls in config urls

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("blog.urls")),
+    path("accounts/", include("users.urls")),
     path("admin/", admin.site.urls),
 ]
