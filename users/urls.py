cdf1a300075a4bd376d2ae2df519cfa38f287000
san5216 <mster.norris@gmail.com>
2025-07-26 23:21:42 -0500

    Fix logout function

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
@@ -7,5 +8,5 @@ app_name = "users"
 
 urlpatterns = [
     path("login/", views.UserLoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
